f719fd96965fe0f9497f4ed8722db6a982e7fbb0
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-02-22 15:27:10 +0900

    ✅ test: compareWinningNumbers 테스트 코드 추가

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,3 +1,4 @@
+import { BONUS, WINNING } from "../src/constants/constant.js";
 import Lotto from "../src/domain/Lotto.js";
 import purchaseLottoCount from "../src/domain/purchaseLottoCount.js";
 
@@ -19,5 +20,21 @@ describe("로또 도메인 테스트", () => {
   test("로또 번호는 오름차순으로 정렬된다.", () => {
     const lotto = new Lotto(reverseLottoNumber);
     expect(lotto.lottoNumbers).toEqual(lottoNumber);
+  });
+  test("당첨 번호 6개가 모두 일치하면 winningCount는 6, bonusCount는 0", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const answerLotto = { 1: WINNING, 2: WINNING, 3: WINNING, 4: WINNING, 5: WINNING, 6: WINNING };
+
+    const result = lotto.compareWinningNumbers(answerLotto);
+
+    expect(result).toEqual({ winningCount: 6, bonusCount: 0 });
+  });
+  test("5개의 숫자가 당첨되고 1개가 보너스 번호인 경우", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 7]); // 7번이 보너스 번호
+    const answerLotto = { 1: WINNING, 2: WINNING, 3: WINNING, 4: WINNING, 5: WINNING, 7: BONUS };
+
+    const result = lotto.compareWinningNumbers(answerLotto);
+
+    expect(result).toEqual({ winningCount: 5, bonusCount: 1 });
   });
 });
