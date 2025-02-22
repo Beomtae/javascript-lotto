140ae573c008e15e57f313dcfa29985d1bd5a605
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-02-22 15:19:50 +0900

    ♻️ refactor: 상수화

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -1,3 +1,4 @@
+import { LOTTO_NUMBER_END, LOTTO_NUMBER_START } from "../src/constants/constant";
 import LottoMachine from "../src/domain/LottoMachine";
 import createSixRandomNumbers from "../src/domain/createSixRandomNumbers";
 
@@ -5,7 +6,7 @@ test("1~45 중 6개의 랜덤 값을 생성한다.", () => {
   const randomValue = createSixRandomNumbers();
   console.log(randomValue);
   expect(randomValue).toHaveLength(6);
-  expect(randomValue.every((num) => num >= 1 && num <= 45)).toBe(true);
+  expect(randomValue.every((num) => num >= LOTTO_NUMBER_START && num <= LOTTO_NUMBER_END)).toBe(true);
 });
 
 test("주어진 count 값만큼 난수(6개의 랜덤값) 세트가 생성된다.", () => {
