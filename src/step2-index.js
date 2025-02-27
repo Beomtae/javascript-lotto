99cddfb4edb4f6cb7e17d63642dbb54cd1d7b50f
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-02-27 15:34:53 +0900

    ✨ feat: 당첨결과 및 수익률 출력 도메인 연결

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,6 @@
+import generateAnswerLotto from "./domain/AnswerLottoPack.js";
 import LottoMachine from "./domain/LottoMachine.js";
+import profitCalculator from "./domain/profitCalculator.js";
 
 /**
  * step 2의 시작점이 되는 파일입니다.
@@ -12,24 +14,23 @@ const resultSection = document.querySelector(".result_section");
 const winningSection = document.querySelector(".winning_number_section");
 
 purchaseButton.addEventListener("click", () => {
-  lottoStart();
+  const { purchaseAmount, lottoPack } = lottoStart();
   resultSection.style.display = "block";
   winningSection.style.display = "block";
-});
+  resultButton.addEventListener("click", () => {
+    checkStart(purchaseAmount, lottoPack);
+    modal.style.display = "block"; // 모달창 보이기
 
-resultButton.addEventListener("click", () => {
-  checkStart();
-  modal.style.display = "block"; // 모달창 보이기
-
-  closeButton.addEventListener("click", () => {
-    modal.style.display = "none";
+    closeButton.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
   });
 });
 
 const lottoStart = () => {
   // 구입 금액 입력 도메인 연결
-  const value = document.querySelector(".purchase_input").value;
-  const { count, lottoPack } = LottoMachine(value);
+  const purchaseAmount = document.querySelector(".purchase_input").value;
+  const { count, lottoPack } = LottoMachine(purchaseAmount);
   const resultText = document.querySelector(".result_text");
   resultText.textContent = `총 ${count}개를 구매하였습니다.`;
 
@@ -45,13 +46,30 @@ const lottoStart = () => {
     `;
     lottoPackContainer.appendChild(lottoElement);
   });
+  return { purchaseAmount, lottoPack };
 };
 
-const checkStart = () => {
+const checkStart = (purchaseAmount, lottoPack) => {
   // 당첨 번호 도메인 연결
   const winningNumberInputs = document.querySelectorAll(".winning_number_input");
   const winningNumbers = [...winningNumberInputs].map((winningNumber) => {
     return Number(winningNumber.value);
   });
   const bonusNumberInput = Number(document.querySelector(".bonus_number_input").value);
+  const answerLotto = generateAnswerLotto(winningNumbers, bonusNumberInput);
+  const winningResult = lottoPack.playCompare(answerLotto);
+  console.log(answerLotto, winningResult);
+  const matchCountFifth = document.getElementById("match_3");
+  const matchCountForth = document.getElementById("match_4");
+  const matchCountThird = document.getElementById("match_5");
+  const matchCountSecond = document.getElementById("match_5+1");
+  const matchCountFirst = document.getElementById("match_6");
+  matchCountFifth.textContent = `${winningResult[3]}개`;
+  matchCountForth.textContent = `${winningResult[4]}개`;
+  matchCountThird.textContent = `${winningResult[5]}개`;
+  matchCountSecond.textContent = `${winningResult["5+1"]}개`;
+  matchCountFirst.textContent = `${winningResult[6]}개`;
+  const profitRate = profitCalculator(purchaseAmount, winningResult);
+  const profitResult = document.querySelector(".profit");
+  profitResult.textContent = `당신의 총 수익률은 ${profitRate}%입니다.`;
 };
